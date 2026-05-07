0fcd58f53a6803cc6445f84b6e2ada1aaa87ca6a
BelkaAlyenka <alakrinskaaalena@gmail.com>
2026-05-07 14:37:59 +0700

    Подключена база данных

--- school/src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
+++ school/src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -1,59 +1,51 @@
 package ru.hogwarts.school.service;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Objects;
+import java.util.*;
 
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.FacultyRepository;
 
 @Service
 public class FacultyServiceImpl implements FacultyService {
 
-    private final HashMap<Long, Faculty> faculties = new HashMap<>();
-    private long count = 0;
+    private final FacultyRepository facultyRepository;
+
+    public FacultyServiceImpl(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
 
     @Override
     public Faculty addFaculty(Faculty faculty) {
-        if (faculty == null) {
-            return null;
-        }
-        faculty.setId(count++);
-        faculties.put(faculty.getId(), faculty);
-        return faculty;
+        return facultyRepository.save(faculty);
     }
 
     @Override
     public Faculty findFaculty(long id) {
-        return faculties.get(id);
+        return facultyRepository.findById(id).orElse(null);
     }
 
     @Override
     public Faculty editFaculty(Faculty faculty) {
-        if (faculty == null || !faculties.containsKey(faculty.getId())) {
-            return null;
-        }
-        faculties.put(faculty.getId(), faculty);
-        return faculty;
+        return facultyRepository.save(faculty);
     }
 
     @Override
     public Faculty deleteFaculty(long id) {
-        return faculties.remove(id);
-    }
-
-    public Collection<Faculty> findByColor(String color) {
-        ArrayList<Faculty> result = new ArrayList<>();
-        for (Faculty faculty : faculties.values()) {
-            if (Objects.equals(faculty.getColor(), color)) {
-                result.add(faculty);
-            }
+        Faculty faculty = findFaculty(id);
+        if (faculty != null) {
+            facultyRepository.deleteById(id);
         }
-        return result;
+        return faculty;
     }
 
+    @Override
+    public Collection<Faculty> findByColor(String color) {
+        return facultyRepository.findByColor(color);
+    }
+
+    @Override
     public Collection<Faculty> findAll() {
-        return new ArrayList<>(faculties.values());
+        return facultyRepository.findAll();
     }
 }
