9719809d20de3916d9faf2d3bac83a50f9ba7674
BelkaAlyenka <alakrinskaaalena@gmail.com>
2026-05-27 00:25:15 +0700

    Домашнее задание урока 3.5. Потоки данных. Работа с файлами

--- school/src/main/java/ru/hogwarts/school/service/StudentService.java
+++ school/src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -3,6 +3,10 @@ package ru.hogwarts.school.service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
+
+import java.io.IOException;
 import java.util.Collection;
 
 public interface StudentService {
@@ -15,4 +19,7 @@ public interface StudentService {
     Collection<Student> findByAgeBetween(int min, int max);
     Collection<Student> findAll();
     Faculty getFacultyByStudentId(Long studentId);
+
+    Avatar findAvatar(long studentId);
+    void uploadAvatar(Long studentId, MultipartFile file) throws IOException;
 }
--- school/src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
+++ school/src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
@@ -1,31 +1,45 @@
 package ru.hogwarts.school.service;
 
-import java.util.*;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
+import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.AvatarRepository;
 import ru.hogwarts.school.repository.StudentRepository;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Collection;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 
-    private final StudentRepository studentRepository;
+    @Value("${avatars.dir.path}")
+    private String avatarsDir;
 
-    public StudentServiceImpl(StudentRepository studentRepository) {
+    private final StudentRepository studentRepository;
+    private final AvatarRepository avatarRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository, AvatarRepository avatarRepository) {
         this.studentRepository = studentRepository;
+        this.avatarRepository = avatarRepository;
     }
 
     @Override
     public Student addStudent(Student student) {
+        student.setId(null);
         return studentRepository.save(student);
     }
 
     @Override
     public Student findStudent(long id) {
-        return studentRepository.findById(id).orElse(null);
+        return studentRepository.findById(id).orElseThrow();
     }
 
     @Override
@@ -65,5 +79,43 @@ public class StudentServiceImpl implements StudentService {
         return studentRepository.findById(studentId)
                 .map(Student::getFaculty)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Студент не найден"));
+    }
+
+    @Override
+    public Avatar findAvatar(long studentId) {
+        return avatarRepository.findByStudentId(studentId).orElseThrow();
+    }
+
+    @Override
+    public void uploadAvatar(Long studentId, MultipartFile file) throws IOException {
+        Student student = findStudent(studentId);
+
+        Path filePath = Path.of(avatarsDir, studentId + "." + getExtension(file.getOriginalFilename()));
+        Files.createDirectories(filePath.getParent());
+        Files.deleteIfExists(filePath);
+
+        try (InputStream is = file.getInputStream();
+             OutputStream os = Files.newOutputStream(filePath, StandardOpenOption.CREATE_NEW);
+             BufferedInputStream bis = new BufferedInputStream(is, 1024);
+             BufferedOutputStream bos = new BufferedOutputStream(os, 1024)
+        ) {
+            bis.transferTo(bos);
+        }
+
+        Avatar avatar = avatarRepository.findByStudentId(studentId).orElseGet(Avatar::new);
+        avatar.setStudent(student);
+        avatar.setFilePath(filePath.toString());
+        avatar.setFileSize(file.getSize());
+        avatar.setMediaType(file.getContentType());
+        avatar.setData(file.getBytes());
+
+        avatarRepository.save(avatar);
+    }
+
+    private String getExtension(String fileName) {
+        if (fileName == null || !fileName.contains(".")) {
+            return "png";
+        }
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 }
